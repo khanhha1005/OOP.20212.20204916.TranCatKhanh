359fe1da69db34fc73ceeb1a03d094bbbe0f182b
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2022-05-24 21:33:53 +0700

    Add files via upload

---
+++ OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
@@ -0,0 +1,25 @@
+package hust.soict.dsai.garbage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+public class GarbageCreator {
+	public static void main(String[] args) {
+		String filename = "";
+		byte[] inputBytes = {0};
+		long startTime, endTime;
+		
+		try {
+			inputBytes = Files.readAllBytes(Paths.get(filename));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		startTime = System.currentTimeMillis();
+		String outputString = "";
+		for (byte b: inputBytes) {
+			outputString += (char)b;
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
+	}
+
+}
