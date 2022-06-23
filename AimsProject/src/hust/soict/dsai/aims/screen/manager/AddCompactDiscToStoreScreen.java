f488754a2227e914cc84cb01b193bb3712f4cce0
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2022-06-23 22:00:14 +0700

    Add files via upload

--- AimsProject/src/hust/soict/dsai/aims/screen/manager/AddCompactDiscToStoreScreen.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/manager/AddCompactDiscToStoreScreen.java
@@ -1,5 +1,102 @@
 package hust.soict.dsai.aims.screen.manager;
 
-public class AddCompactDiscToStoreScreen extends AddItemsToStoreScreen{
+import java.awt.BorderLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import hust.soict.dsai.aims.Aims.Store;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.Track;
+
+
+public class AddCompactDiscToStoreScreen extends AddItemsToStoreScreen {
+
+	JButton btnAdd = new JButton("Add");
+	JButton btnAddTracks = new JButton("Add Tracks");
+    private ArrayList<Track> tracks= new ArrayList<Track>();
+
+	CompactDisc disc;
+	String director, length, artist;
+	public AddCompactDiscToStoreScreen(Store store , JPanel storeScreenCenter) throws HeadlessException {
+		super(store, storeScreenCenter);
+		JPanel director = super.createSubPanel("Director");
+		JPanel length = super.createSubPanel("Length");
+		JPanel artist = super.createSubPanel("Artist");
+		super.centerPanel.add(director, BorderLayout.CENTER);
+		super.centerPanel.add(length, BorderLayout.CENTER);
+		super.centerPanel.add(artist, BorderLayout.CENTER);
+		
+		JButton btnAdd = new JButton("Add CD");
+		btnAdd.addActionListener(new btnListener());
+		super.centerPanel.add(btnAdd,BorderLayout.SOUTH);
+		
+		JButton btnAddTracks = new JButton("Add Tracks");
+		btnAddTracks.addActionListener(new btnListener());
+		super.centerPanel.add(btnAddTracks,BorderLayout.CENTER);
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.addActionListener(new btnListener());
+		super.centerPanel.add(btnReset, BorderLayout.WEST);	
+		
+		super.setTitle("Add CD");
+	}
+	
+	private class btnListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (e.getActionCommand().equals("Add CD")) {
+				for (JTextField tf : textFields) {
+					if (tf.getName().equals("Title")) {
+						title = tf.getText();
+					}
+					if (tf.getName().equals("Cost")) {
+						cost = tf.getText();
+					}
+					if (tf.getName().equals("Category")) {
+						category = tf.getText();
+					}
+					if (tf.getName().equals("Director")) {
+						director = tf.getText();
+					}
+					if (tf.getName().equals("Length")) {
+						length = tf.getText();
+					}
+					if (tf.getName().equals("Artist")) {
+						artist = tf.getText();
+					}
+				} 
+				CompactDisc disc;
+				disc = new CompactDisc(title, category,  director,Integer.parseInt(length), Float.parseFloat(cost) , artist, tracks);
+				store.addMedia(disc);
+				StoreScreenCenter.add(new MediaStore(disc));
+				StoreScreenCenter.repaint();
+				StoreScreenCenter.revalidate();
+			} else if (e.getActionCommand().equals("Add CD")) {
+				for (JTextField tf : textFields) {
+					if (tf.getName().equals("Length")) {
+						length = tf.getText();
+					}
+					if (tf.getName().equals("Title")) {
+						title = tf.getText();
+					}
+				}
+				Track track;
+				track = new Track(title,Integer.parseInt(length));
+				disc.addTracks(track);
+			} else {
+				for(JTextField tf: textFields) {
+					tf.setText("");
+				}
+			}
+
+		}
+	}
 }
--- AimsProject/src/hust/soict/dsai/aims/screen/manager/MediaStore.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/manager/MediaStore.java
@@ -3,38 +3,72 @@ package hust.soict.dsai.aims.screen.manager;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
+import hust.soict.dsai.swing.NumberGrid.ButtonListener;
+import hust.soict.dsai.swing.SwingAccumulator.TFInputListener;
 
-public class MediaStore extends JPanel{
-private Media media;
-public MediaStore(Media media) {
-	this.media = media;
-	this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-	JLabel title = new JLabel(media.getTitle());
-	title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 15));
-	title.setAlignmentX(CENTER_ALIGNMENT);
-	JLabel cost = new JLabel(""+media.getCost()+" $");
-	cost.setAlignmentX(CENTER_ALIGNMENT) ;
-	JPanel container = new JPanel();
-	container.setLayout(new FlowLayout(FlowLayout.CENTER));
-	if(media instanceof Playable) {	
-		JButton playButton = new JButton("Play");
-		container.add(playButton);
+public class MediaStore extends JPanel implements ActionListener {
+	private Media media;
+    static JFrame f;
+
+	public MediaStore(Media media) {
+		this.media = media;
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JLabel title = new JLabel(media.getTitle());
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 15));
+		title.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel cost = new JLabel(""+media.getCost()+" $");
+		cost.setAlignmentX(CENTER_ALIGNMENT) ;
+		JPanel container = new JPanel();
+		container.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		if(media instanceof Playable) {	
+			JButton playButton = new JButton("Play");
+			container.add(playButton);
+
+	        playButton.addActionListener(new TFInputListener());
+
+		}
+
+		this.add(Box.createVerticalGlue());
+		this.add(title);
+		this.add(cost);
+		this.add(Box.createVerticalGlue());
+		this.add(container);
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	}
-	this.add(Box.createVerticalGlue());
-	this.add(title);
-	this.add(cost);
-	this.add(Box.createVerticalGlue());
-	this.add(container);
-	this.setBorder(BorderFactory.createlineBorder(Color.BLACK));
+
+	private class TFInputListener implements ActionListener { 
+	
+		public void actionPerformed(ActionEvent e) 
+		    {
+		        String s = e.getActionCommand();
+		        if (s.equals("Play")) {
+		            JDialog d = new JDialog(f, "Play Box");
+		            JLabel l = new JLabel("The Media "+media.getTitle()+" is playing",SwingConstants.CENTER);
+		    		l.setFont(new Font(l.getFont().getName(), Font.PLAIN, 15));
+
+		            d.add(l);
+		            d. setLocationRelativeTo(null);        
+		            d.setSize(500, 200);
+		 
+		            d.setVisible(true);
+		        }
+		}
+		
 	}
 }
